fe5b9f176d144a982eb9bf61555a16e70a0fe466
sagreta86 <kingofdarkness8@gmail.com>
2023-06-24 16:02:45 -0700

    Changed

--- bloom.cpp
+++ bloom.cpp
@@ -1,55 +1,72 @@
+#ifndef BLOOM_FILTER_H
+#define BLOOM_FILTER_H
+
 #include <vector>
-#include "BloomFilter.h"
-#include "smhasher/MurmurHash3.h"
+#include <array>
 
-template< class Key, class Hash = std::hash<Key> >
+#include "MurmurHash3.h"
+
+//basic structure of a bloom filter object
 struct BloomFilter {
-	BloomFilter(uint64_t size, uint8_t numHashes);
-
-	void add(const uint8_t* data, std::size_t len);
-	bool possiblyContains(const uint8_t* data, std::size_t len) const;
-
+    BloomFilter(size_t size, uint8_t numHashes);
+    void add(const uint8_t* data, std::size_t len);
+    bool possiblyContains(const uint8_t* data, std::size_t len) const;
 private:
-	uint8_t m_numHashes;
-	std::vector<bool> m_bits;
+    uint8_t m_numHashes;
+    std::vector<bool> m_bits;
 };
-
-BloomFilter::BloomFilter(uint64_t size, uint8_t numHashes)
-	: m_bits(size),
-	m_numHashes(numHashes) {}
-
-std::array<uint64_t, 2> hash(const uint8_t* data,
-	std::size_t len) {
-	std::array<uint64_t, 2> hashValue;
-	MurmurHash3_x64_128(data, len, 0, hashValue.data());
-
-	return hashValue;
+//Bloom filter constructor
+BloomFilter::BloomFilter(size_t size, uint8_t numHashes)
+    : m_bits(size),
+    m_numHashes(numHashes) {}
+//Hash array created using the MurmurHash3 code
+static std::array<uint64_t, 2> myhash(const uint8_t* data, std::size_t len)
+{
+    std::array<uint64_t, 2> hashValue{};
+    MurmurHash3_x64_128(data, len, 0, hashValue.data());
+    return hashValue;
 }
-
-inline uint64_t nthHash(uint8_t n,
-	uint64_t hashA,
-	uint64_t hashB,
-	uint64_t filterSize) {
-	return (hashA + n * hashB) % filterSize;
+//Hash array created using the MurmurHash3 code
+inline size_t nthHash(int n,
+    uint64_t hashA,
+    uint64_t hashB,
+    size_t filterSize) {
+    return (hashA + n * hashB) % filterSize; // <- not sure if that is OK, perhaps it is.
 }
-
+//Adds an element to the array
 void BloomFilter::add(const uint8_t* data, std::size_t len) {
-	auto hashValues = hash(data, len);
-
-	for (int n = 0; n < m_numHashes; n++) {
-		m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())] = true;
-	}
+    auto hashValues = myhash(data, len);
+    for (int n = 0; n < m_numHashes; n++)
+    {
+        m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())] = true;
+    }
 }
-
-bool BloomFilter::possiblyContains(const uint8_t* data, std::size_t len) const {
-	auto hashValues = hash(data, len);
-
-	for (int n = 0; n < m_numHashes; n++) {
-		if (!m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())]) {
-			return false;
-		}
-	}
-
-	return true;
+//Returns true or false based on a probabilistic assesment of the array         using MurmurHash3
+bool BloomFilter::possiblyContains(const uint8_t* data, std::size_t   len) const {
+    auto hashValues = myhash(data, len);
+    for (int n = 0; n < m_numHashes; n++)
+    {
+        if (!m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())])
+        {
+            return false;
+        }
+    }
+    return true;
 }
+#endif
 
+#include <functional>
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    BloomFilter bf(1024 * 1024, 5);
+    std::string s = "12345";
+    bf.add((uint8_t*)s.c_str(), s.size());
+    bool possiblyContains = bf.possiblyContains((uint8_t*)s.c_str(), s.size());
+    cout << "possible: " << possiblyContains << endl;
+    assert(possiblyContains);
+}
